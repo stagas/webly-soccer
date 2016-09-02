03fd186fad12d80dd6c00cebd15f010e2038f8fc
George Stagas <gstagas@gmail.com>
2016-09-02 18:15:44 +0300

    add camera

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ require('./style.css');
 var Loop = require('./lib/loop');
 var arrows = require('./lib/arrows');
 var Stadium = require('./src/stadium');
+var Camera = require('./src/camera');
 var Player = require('./src/player');
 
 var k = arrows(document.body);
 
 var stadium = new Stadium;
 var player = new Player;
+var camera = new Camera(player);
 document.body.appendChild(stadium.el);
 document.body.appendChild(player.el);
-
+player.pos.x = player.px.x = 2300 / 2 - player.width * player.scale / 2;
+player.pos.y = player.px.y = 1200 / 2 - player.height * player.scale;
 /* loop */
 
 var loop = new Loop;
@@ -26,10 +29,12 @@ loop.fps(10).start();
 
 function update() {
   player.update();
+  camera.update();
 }
 
 function render(dt, alpha, frame, elapsed) {
-  player.render(dt, alpha, frame, elapsed);
+  player.render(dt, alpha);
+  camera.render(dt, alpha)
 }
 
 function controls() {
---
+++ src/camera.js
@@ -0,0 +1,41 @@
+var Point = require('../lib/point');
+
+module.exports = Camera;
+
+function Camera(leader) {
+  this.leader = leader;
+  this.speed = 0.13;
+  this.friction = 0.49;
+  this.px = new Point;
+  this.pos = new Point;
+  this.vel = new Point;
+  this.size = new Point;
+  this.onresize();
+  window.onresize = this.onresize.bind(this);
+}
+
+Camera.prototype.onresize = function() {
+  this.size.x = window.innerWidth;
+  this.size.y = window.innerHeight;
+};
+
+Camera.prototype.update = function() {
+  var dx = (this.leader.pos.x + this.leader.width * this.leader.scale / 2 - this.size.x / 2) - this.pos.x;
+  var dy = (this.leader.pos.y + this.leader.height * this.leader.scale / 2 - this.size.y / 2) - this.pos.y;
+
+  this.vel.x += dx * this.speed;
+  this.vel.y += dy * this.speed;
+
+  this.pos.x += this.vel.x;
+  this.pos.y += this.vel.y;
+
+  this.vel.x *= this.friction;
+  this.vel.y *= this.friction;
+};
+
+Camera.prototype.render = function(dt, alpha) {
+  this.px.x += (this.pos.x - this.px.x) * alpha;
+  this.px.y += (this.pos.y - this.px.y) * alpha;
+
+  window.scrollTo(this.px.x, this.px.y);
+};
